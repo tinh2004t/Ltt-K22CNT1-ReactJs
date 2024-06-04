f4e490a7746be7483d482b4b77537d760b2d8477
tinh2004t <117645071+tinh2004t@users.noreply.github.com>
2024-06-04 10:30:10 +0700

    123123

--- Lesson06/ltt-lesson06-hook/src/App.js
+++ Lesson06/ltt-lesson06-hook/src/App.js
@@ -1,43 +1,79 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import LttListTask from './components/LttListTask';
 import LttTaskAddOrEdit from './components/LttTaskAddOrEdit';
 
 function App() {
   const ltt_listTasks = [
-    { ltt_taskId:2210900130,ltt_taskName:"Le Tuan Tinh", ltt_level:"Small" },
-    { ltt_taskId:2210900130,ltt_taskName:"Học lập trình frontend", ltt_level:"Small" },
-    { ltt_taskId:2, ltt_taskName:"Học lập trình reactjs",ltt_level:"Medium"},
-    { ltt_taskId:3, ltt_taskName:"Lập trình với Frontend - Reactjs",ltt_level:"High"},
-    { ltt_taskId:4, ltt_taskName:"Lập trình Fullstack (PHP, Java, NetCore)",ltt_level:"Small"},
-   ];
+    { ltt_taskId: 2210900130, ltt_taskName: "Le Tuan Tinh", ltt_level: "Small" },
+    { ltt_taskId: 2210900130, ltt_taskName: "Học lập trình frontend", ltt_level: "Small" },
+    { ltt_taskId: 2, ltt_taskName: "Học lập trình reactjs", ltt_level: "Medium" },
+    { ltt_taskId: 3, ltt_taskName: "Lập trình với Frontend - Reactjs", ltt_level: "High" },
+    { ltt_taskId: 4, ltt_taskName: "Lập trình Fullstack (PHP, Java, NetCore)", ltt_level: "Small" },
+  ];
 
-   // su dung hang useState de luu tru trang thai du lieu
-   const [lttListTask, setLttListTask] = useState(ltt_listTasks);
+  // su dung hang useState de luu tru trang thai du lieu
+  const [lttListTask, setLttListTask] = useState(ltt_listTasks);
 
-   const lttHandleSubmit = (lttParam) => {
-      console.log("App:", lttParam);
+
+
+  //Ham xu ly don du lieu sua
+  // luu tru state
+  const task = { ltt_taskId: 2210900130, ltt_taskName: "NTU", ltt_level: "Small" }
+  const [lttTask, setLttTask] = useState(task);//dung de render form
+
+  // quan ly trang thai form khi them / sua
+  const [lttAddOrEdit, setLttAddOrEdit] = useState(false); //Them  
+
+  const lttHandleEdit = (param) => {
+    setLttTask(param);
+    setLttAddOrEdit(true);
+  }
+
+
+
+  const lttHandleSubmit = (lttParam) => {
+    console.log("App:", lttParam);
+    if (lttAddOrEdit === false) { //khi them moi
       setLttListTask(prev => {
-        return[
+        return [
           ...prev,
           lttParam
         ]
       })
-   }
+    }else {//submit khi sua
+      console.log("App Edit:", lttParam);
+      for (let i = 0; i < lttListTask.length; i++) {
+        if (lttListTask[i].ltt_taskId === lttParam.ltt_taskId) {
+          console.log("lttListTask Edit:",i,lttListTask[i].ltt_taskId);
+          lttListTask[i] = lttParam;
+        }
+        console.log(lttListTask);
+        setLttAddOrEdit(LttListTask);
+      }
+    
+    }
+    useEffect(() => {
+      setLttListTask(lttListTask);
+    },[lttlistTask]);
+  }
+   
   return (
     <div className="container border">
       <h1>Le Tuan Tinh - K22CNT1</h1>
-      <hr/>
+      <hr />
       <div>
         {/*Danh sach list Task*/}
-        <LttListTask renderLttListTasks = {lttListTask}/>
+        <LttListTask renderLttListTasks={lttListTask} onLttEdit={lttHandleEdit} />
       </div>
       <div>
-        <LttTaskAddOrEdit lttOnSubmit={lttHandleSubmit}/>
+        <LttTaskAddOrEdit lttOnSubmit={lttHandleSubmit}
+          renderLttAddOrEdit={lttAddOrEdit}
+          renderLttTask={lttTask} />
       </div>
     </div>
-    
+
   );
 }
 
--- Lesson06/ltt-lesson06-hook/src/components/LttListTask.js
+++ Lesson06/ltt-lesson06-hook/src/components/LttListTask.js
@@ -1,20 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-export default function LttListTask({renderLttListTasks}) {
-    console.log(renderLttListTasks);
+export default function LttListTask({ renderLttListTasks, onLttEdit }) {
+    console.log("List:",renderLttListTasks);
+
+    const [lttList, setLttList] = useState(renderLttListTasks);
+    useEffect(() => {
+       setLttList(renderLttListTasks) 
+    },[renderLttListTasks]);
+
+    //Ham xu ly du lieu khi edit
+    const lttHandleEdit = (param) => {
+        console.log("Edit", param);
+        //chuyen du lieu sua len app
+        onLttEdit(param);
+
+    }
 
     //render data
 
-    let lttElementTask = renderLttListTasks.map((task, index) => {
+    let lttElementTask = lttList.map((task, index) => {
         return (
             <>
                 <tr key={index}>
-                    <td>{index +1}</td>
+                    <td>{index + 1}</td>
                     <td>{task.ltt_taskId}</td>
                     <td>{task.ltt_taskName}</td>
-                    <td>{task.ltt_taskId}</td>
+                    <td>{task.ltt_level}</td>
                     <td>
-                        <button className='btn btn-success'>Edit</button>
+                        <button className='btn btn-success' onClick={() => lttHandleEdit(task)}>Edit</button>
                         <button className='btn btn-danger'>Remove</button>
                     </td>
                 </tr>
@@ -23,11 +36,11 @@ export default function LttListTask({renderLttListTasks}) {
         )
     })
 
-  return (
-    <div>
-        <h2>Danh sach cac nhiem vu</h2>
-        <table className='table table-bordered'>
-            <thead>
+    return (
+        <div>
+            <h2>Danh sach cac nhiem vu</h2>
+            <table className='table table-bordered'>
+                <thead>
                     <tr>
                         <th>STT</th>
                         <th>Task ID</th>
@@ -35,12 +48,12 @@ export default function LttListTask({renderLttListTasks}) {
                         <th>Task Level</th>
                         <th>Action</th>
                     </tr>
-            </thead>
+                </thead>
 
-            <tbody>
-                {lttElementTask}
-            </tbody>
-        </table>
-    </div>
-  )
+                <tbody>
+                    {lttElementTask}
+                </tbody>
+            </table>
+        </div>
+    )
 }
